ac246e460184f71079119bc00f9d6998ab7a0a2b
nadadroussi <114990780+nadadroussi@users.noreply.github.com>
2024-12-14 20:06:39 +0100

    add the API code

--- API/model_integration.py
+++ API/model_integration.py
@@ -1,10 +1,21 @@
+from llm.utils import get_pdf_text, get_text_chunks, get_vector_store, get_conversation_chain
 
+def generate_answer(question):
+    # Path to the PDF file
+    pdf_docs = ['../data/CoursOptimisation.pdf']
 
-# Simulated model integration function
-def generate_answer(question: str) -> str:
-    """
-    Generating the answer     #Nada
-    """
-    # Simulating answer generation (replace with actual model logic)
-    simulated_answer = f"Simulated answer to the question: '{question}'"
+    # Extract text from the PDF
+    raw_text = get_pdf_text(pdf_docs)
+
+    # Split the text into chunks
+    text_chunks = get_text_chunks(raw_text)
+
+    # Create the vector store
+    vector_store = get_vector_store(text_chunks)
+
+    # Create the conversation chain
+    conversation = get_conversation_chain(vector_store)
+
+    # Get the answer from the conversation chain
+    simulated_answer = conversation.run(question)
     return simulated_answer
---
+++ llm/app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+import os
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.chat_models import ChatOllama
+from utils import get_pdf_text, get_text_chunks, get_vector_store, get_conversation_chain
+
+
+def conv():
+    # get the extracted text from multiple pdfs
+    pdf_docs = ['../data/CoursOptimisation.pdf']
+    raw_text = get_pdf_text(pdf_docs)
+    # print(raw_text)
+
+
+    # get the text chunks
+    text_chunks = get_text_chunks(raw_text)
+    # print(text_chunks)
+
+
+    # create the vector store 
+    vector_store = get_vector_store(text_chunks)
+    
+    # create conversation chain
+    conversation = get_conversation_chain(vector_store)
+
+
+    # Example
+    question = "What is this data science?"
+
+    # Get the answer from the conversation chain
+    try:
+        answer = conversation.run(question)
+        print(f"Question: {question}")
+        print(f"Answer: {answer}")
+        return answer
+    except Exception as e:
+        print(f"Error during conversation: {e}")
+        return None
+
+
+# if __name__ == '__main__':
+#     # Call the main function
+#     final_answer = main()
+#     if final_answer:
+#         print("Final Answer:")
+#         print(final_answer)
